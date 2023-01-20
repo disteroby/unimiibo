4a16032f4eadbdb27d1fa65a845b0b2ad0d1c8d7
Roberto Di Stefano <r.distefano4@campus.unimib.it>
2023-01-20 16:13:42 +0100

    Added waves and fonts adjustments

--- src/views/About/About.js
+++ src/views/About/About.js
@@ -4,8 +4,34 @@ import './About.css'
 function About() {
     //TODO
     return (
-        <div className="d-flex justify-content-center">
-            <UnimiiboLoading text="Work in progress"/>
+        <div>
+            <div className="d-flex justify-content-center">
+                <UnimiiboLoading text="Work in progress"/>
+            </div>
+
+            {/*<div className="container this">*/}
+            {/*    <div className="row">*/}
+            {/*        <div className="col-12 col-md-6">*/}
+            {/*            <div className="question-wrapper">*/}
+            {/*                <div className="question-graphics">*/}
+            {/*                    <div className="question-icon" style={{color:'slategrey', fontSize:'4rem'}}><i className="bi bi-patch-question"></i></div>*/}
+            {/*                    <div className="question-line"></div>*/}
+            {/*                </div>*/}
+            {/*                <div className="question-texts">*/}
+            {/*                    <div>*/}
+            {/*                        What do I need in order to play with amiibo?*/}
+            {/*                    </div>*/}
+            {/*                    <div>*/}
+            {/*                        You will need an amiibo figure or card, a compatible game, and a Nintendo Switch, Wii U,  New Nintendo 3DS XL, or New Nintendo 2DS XL system.*/}
+            {/*                        For use with Nintendo 3DS, Nintendo 3DS XL and Nintendo 2DS systems, a Nintendo 3DS NFC Reader/Writer accessory is required. (Sold separately).*/}
+            {/*                    </div>*/}
+            {/*                </div>*/}
+            {/*            </div>*/}
+            {/*        </div>*/}
+            {/*    </div>*/}
+            {/*</div>*/}
+
+
         </div>
     );
 }
